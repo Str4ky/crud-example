f5408a0f0071b9ef268dcb8463ac99e91eb53375
Tim <38327800+Str4ky@users.noreply.github.com>
2024-01-29 16:32:08 +0100

    Add files via upload

--- assets/js/query.js
+++ assets/js/query.js
@@ -2,6 +2,7 @@
 let search = document.getElementById('search');
 let button = document.getElementById('button');
 let filter = document.getElementById('filter');
+let element;
 
 //Si la valeur de recherche a une modification on appel la fonction updateValue
 search.addEventListener("input", updateValue);
@@ -9,19 +10,25 @@ search.addEventListener("input", updateValue);
 //Si on appuie sur entrée on redirige vers la page avec la recherche
 search.addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
-        let element = filter.options[filter.selectedIndex].value;
+        //Définir la variable du filtre sélectionné
+        element = filter.options[filter.selectedIndex].value;
+        //Rediriger vers la page avec la recherche
         window.location.href = "?search=" + e.target.value + "&filter=" + element;
     }
 });
 
 //Fonction updateValue
 function updateValue(e) {
-    let element = filter.options[filter.selectedIndex].value;
+    //Définir la variable du filtre sélectionné
+    element = filter.options[filter.selectedIndex].value;
+    //Remplacer l'attribut href du bouton par la nouvelle recherche
     button.href = "?search=" + e.target.value + "&filter=" + element;
 };
 
 //Fonction updateFilter qui change le filtre selon sa sélection en temps réel
 function updateFilter() {
-    let element = filter.options[filter.selectedIndex].value;
+    //Définir la variable du filtre sélectionné
+    element = filter.options[filter.selectedIndex].value;
+    //Remplacer l'attribut href du bouton par la nouvelle recherche
     button.href = "?search=" + search.value + "&filter=" + element;
 }
