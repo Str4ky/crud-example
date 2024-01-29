ba8b31f84fef88a8317efec3918118325afc0215
Tim <38327800+Str4ky@users.noreply.github.com>
2024-01-29 13:59:07 +0100

    Add files via upload

--- assets/js/query.js
+++ assets/js/query.js
@@ -1,8 +1,12 @@
+//Définir les variables
 let search = document.getElementById('search');
 let button = document.getElementById('button');
 let filter = document.getElementById('filter');
 
+//Si la valeur de recherche a une modification on appel la fonction updateValue
 search.addEventListener("input", updateValue);
+
+//Si on appuie sur entrée on redirige vers la page avec la recherche
 search.addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
         let element = filter.options[filter.selectedIndex].value;
@@ -10,10 +14,12 @@ search.addEventListener("keydown", function(e) {
     }
 });
 
+//Fonction updateValue
 function updateValue(e) {
     button.href = "?search=" + e.target.value + "&filter=" + element;
 };
 
+//Fonction updateFilter qui change le filtre selon sa sélection en temps réel
 function updateFilter() {
     let element = filter.options[filter.selectedIndex].value;
     button.href = "?search=" + search.value + "&filter=" + element;
